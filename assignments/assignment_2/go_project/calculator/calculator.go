fd8c2ec75204b8cfa57a3546a413cb968492de26
spower136 <s.power136@yahoo.com>
2022-06-29 20:24:35 -0700

    finish assignment 2

--- assignments/assignment_2/go_project/calculator/calculator.go
+++ assignments/assignment_2/go_project/calculator/calculator.go
@@ -20,11 +20,15 @@ func Multiply(a, b float64) float64 {
 	return a * b
 }
 
-func Divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, errors.New("division by zero not allowed")
-	}
-	return a / b, nil
+// func Divide(a, b float64) (float64, error) {
+// 	if b == 0 {
+// 		return 0, errors.New("division by zero not allowed")
+// 	}
+// 	return a / b, nil
+// }
+
+func Divide(a, b float64) float64 {
+	return a / b
 }
 
 func F(a, b float64) (float64, error) {
--- assignments/assignment_2/go_project/calculator/calculator_operations/main.go
+++ assignments/assignment_2/go_project/calculator/calculator_operations/main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
-	result := calculator.Add(2, 2)
+	step1 := calculator.Multiply(3, 2)
+	step2 := calculator.Add(step1, 6)
+	step3 := calculator.Multiply(3, 4)
+	step4 := calculator.Add(step3, 2)
+	result := calculator.Divide(step2, step4)
 	fmt.Println(result)
 }
